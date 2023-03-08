e8135f8ec5f649e07c297fd97e42176de89581d7
Arjan Dhakal <dhakal.arjan@gmail.com>
2023-03-08 08:02:36 +0545

    completed exercises 1.33

--- chp-1/ex-1.33.rkt
+++ chp-1/ex-1.33.rkt
@@ -8,6 +8,7 @@
   (find-divisor n 2))
 
 (define (inc x) (+ x 1))
+(define (identity x) x)
 
 (define (find-divisor n test-divisor)
   (cond ((> (square test-divisor) n)
@@ -24,6 +25,25 @@
 (define (prime? n)
   (= n (smallest-divisor n)))
 
+
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (relative-prime? a b)
+  (if (= (gcd a b) 1)
+      #true
+      #false)
+  )
+
+
+
+
+(check-eq? (relative-prime? 2 3) #true)
+(check-eq? (relative-prime? 3 4) #true)
+(check-eq? (relative-prime? 4 8) #false)
 
 
 ; Exercise 1.33
@@ -43,6 +63,14 @@
 
 (check-eq? (sum-squares-prime 1 20) 1028)
 (check-eq? (sum-squares-prime 2 20) 1027)
+
+;Exercise 1.33.2
+; Procedure to calculate product of all the positive integers less than n
+; that are relatively prime to n
+(define (product-relative-prime n)
+  (filter-accumulate * 1 identity 1 inc n
+                     (lambda (x) (relative-prime? x n))
+                     ))
 
 
 
